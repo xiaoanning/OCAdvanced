b485fb7ce5a3a30e7f917ba4b967e9b73358bca2
xiaoanning <734871221@qq.com>
2016-12-24 15:39:49 +0800

    Initial Commit

---
+++ OCAdvanced/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCAdvanced
+//
+//  Created by 安宁 on 16/12/24.
+//  Copyright © 2016年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCAdvanced/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCAdvanced
+//
+//  Created by 安宁 on 16/12/24.
+//  Copyright © 2016年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
